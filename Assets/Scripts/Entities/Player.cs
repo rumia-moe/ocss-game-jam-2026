c740f639fe59e7a75ba5057ad076c82b1f59be81
Jakob de Guzman <jakob@rumia.moe>
2026-09-19 10:37:10 +1200

    object file

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -1,14 +1,6 @@
 public class Player : Entity
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
