f4b960bcd6ad001f99647d4a233ff949bb7a6ec7
FreakzOfCoding <zaneosborne2006@gmail.com>
2026-09-19 11:55:54 +1200

    Added parallax script

--- Assets/Scripts/Entities/EntityAttributes/EntityAttribute.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class EntityAttribute : MonoBehaviour
-{
-
-    public string entityAttributeName = "Entity Attribute";
-
-}
--- Assets/Scripts/Entities/EntitySkills/EntitySkill.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class EntitySkill : MonoBehaviour
-{
-
-    public string entitySkillName = "Entity Skill";
-
-}
