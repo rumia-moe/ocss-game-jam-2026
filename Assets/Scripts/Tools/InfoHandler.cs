068879a1323118ed325a871540ce0eec1ca4e693
FreakzOfCoding <zaneosborne2006@gmail.com>
2026-09-20 09:29:52 +1200

    added info page changer

--- Assets/Scripts/Tools/InfoHandler.cs
+++ Assets/Scripts/Tools/InfoHandler.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 
 public class InfoHandler : MonoBehaviour
 {
+    public TextMeshProUGUI title;
     public TextMeshProUGUI healthValue;
     public TextMeshProUGUI attackValue;
     public TextMeshProUGUI insightValue;
     public TextMeshProUGUI agilityValue;
+
+
+    public void ChangeInfo(Entity muse)
+    {
+        title.text = muse.EntityName;
+        healthValue.text = muse.EntityMaxHealth.ToString();
+        attackValue.text = muse.damage.ToString();
+        insightValue.text = muse.insight.ToString();
+        agilityValue.text = muse.movementSpeed.ToString();
+    }
 
 
 
