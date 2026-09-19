c4cd1bd26abab677af76ddd76f6045eaeb32c9b9
Jakob de Guzman <jakob@rumia.moe>
2026-09-20 00:47:58 +1200

    switch damage collision to ocne

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -66,7 +66,7 @@ public abstract class Entity : MonoBehaviour, IInteractable
 
     public virtual void Interact() { }
 
-    protected virtual void OnCollisionStay2D(Collision2D collision)
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.otherCollider != this.hitboxCollider) return;
 
