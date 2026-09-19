02f58c7659c754ad2f6049038415c41ab8538e36
Jakob de Guzman <jakob@rumia.moe>
2026-09-19 21:12:39 +1200

    start of enchanted coral

---
+++ Assets/Scripts/EnchantedCoral.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnchantedCoral : MonoBehaviour, IInteractable
+{
+
+    public Collider2D selectableCollider;
+
+    public Collider2D SelectableCollider => selectableCollider;
+
+    public void Interact() {
+
+
+
+    }
+
+}
