e1ab615bab37034143a42c7f593ed8b1678d8f2b
FreakzOfCoding <zaneosborne2006@gmail.com>
2026-09-20 03:44:59 +1200

    semblance of a stat sheet

---
+++ Assets/Scripts/Tools/SceneChangerButton.cs
@@ -0,0 +1,21 @@
+using Unity.Tutorials.Editor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChangerButton : MonoBehaviour
+{
+    public Button button;
+    public string sceneName;
+
+    private void Reset() => button = GetComponent<Button>();
+    private void OnEnable() => button.onClick.AddListener(OnClick);
+    private void OnDisable() => button.onClick.RemoveListener(OnClick);
+
+    private void OnClick()
+    {
+        if (string.IsNullOrEmpty(sceneName) )return;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
