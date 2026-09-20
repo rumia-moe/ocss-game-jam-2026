f9364cf39188447d36c3b242a4001e0673050a8a
Gray7915 <patrick.james.gray15@gmail.com>
2026-09-20 12:41:36 +1200

    audio from main menu now, hooray

--- Assets/Scripts/PersistentObject.cs
+++ Assets/Scripts/PersistentObject.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class PersistentObject : MonoBehaviour
 {
+    private static PersistentObject instance;
+
     private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
     }
 }
